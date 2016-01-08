651bcdd0489c3d85597c0a0150661ac1d13e527e
KevinDTran <kevin@zipmark.com>
2016-01-08 17:46:52 -0600

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,6 @@
 def fizzbuzz (int)
   if int % 3 == 0 and int % 5 == 0
-    "FizzBuzz"
-     
+    "FizzBuzz"  
   elsif int % 5 == 0 # if the number int is divisible by 5
     "Buzz" 
   elsif int % 3 == 0 
